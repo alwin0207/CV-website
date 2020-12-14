f676eb5d22a848f3a36e422cae28b57e9d0ea976
Alwin <alwinbuist@gmail.com>
2020-12-14 13:18:58 +0100

    deleted test files

--- oldfiles/schooling.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const SchoolingSchema = new mongoose.Schema({
-    title: String,
-    location: String,
-    description: String,
-    timepoint: String
-  });
-  module.exports = mongoose.model("Schooling", SchoolingSchema);
--- oldfiles/work_exp.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const WorkExpSchema = new mongoose.Schema({
-    title: String,
-    location: String,
-    description: String,
-    timepoint: String
-  });
-  module.exports = mongoose.model("WorkExp", WorkExpSchema);
-  
